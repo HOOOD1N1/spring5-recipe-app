51fa680e6ef96654b342717d633d48301e10778a
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-16 00:13:39 +0200

    Add Database Initialization

--- src/main/java/edy/springframework/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/edy/springframework/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package edy.springframework.spring5recipeapp.repositories;
 import edy.springframework.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
+    Optional<Category> findByDescription(String description);
+ }
