709783d5aacf20854cd0b2a181c7b2e4d467c86f
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-16 02:06:35 +0200

    Refactor with Lombok

--- src/main/java/edy/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/edy/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import edy.springframework.spring5recipeapp.domain.*;
 import edy.springframework.spring5recipeapp.repositories.CategoryRepository;
 import edy.springframework.spring5recipeapp.repositories.RecipeRepository;
 import edy.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-
+@Slf4j
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
@@ -30,6 +31,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loadin bootstrap data ...");
     }
 
     private List<Recipe> getRecipes() {
--- src/main/java/edy/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/edy/springframework/spring5recipeapp/domain/Ingredient.java
@@ -1,9 +1,11 @@
 package edy.springframework.spring5recipeapp.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-
+@Data
 @Entity
 public class Ingredient {
 
@@ -37,35 +39,4 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 }
