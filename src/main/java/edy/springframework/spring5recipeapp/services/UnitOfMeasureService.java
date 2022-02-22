78d923cd8794b52ca110f1ab963d463bc070d3b7
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-22 23:11:15 +0200

    Added update ingredient

---
+++ src/main/java/edy/springframework/spring5recipeapp/services/UnitOfMeasureService.java
@@ -0,0 +1,9 @@
+package edy.springframework.spring5recipeapp.services;
+
+import edy.springframework.spring5recipeapp.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
