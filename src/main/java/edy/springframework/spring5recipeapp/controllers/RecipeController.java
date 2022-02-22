9b11dae9f4aa48363c1028090253f234a9359e3e
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-22 21:23:13 +0200

    Added delete a recipe

--- src/main/java/edy/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/edy/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -2,13 +2,12 @@ package edy.springframework.spring5recipeapp.controllers;
 
 import edy.springframework.spring5recipeapp.commands.RecipeCommand;
 import edy.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -41,7 +40,16 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+
+        log.debug("Deleting id: " + id);
+
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 
 }
