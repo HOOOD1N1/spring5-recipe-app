faba7de3f6cc87b4b8ff449bb1503d47556a3d2b
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-22 23:50:49 +0200

    Added delete ingredient

--- src/main/java/edy/springframework/spring5recipeapp/services/IngredientService.java
+++ src/main/java/edy/springframework/spring5recipeapp/services/IngredientService.java
@@ -3,6 +3,10 @@ package edy.springframework.spring5recipeapp.services;
 import edy.springframework.spring5recipeapp.commands.IngredientCommand;
 
 public interface IngredientService {
+
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long idToDelete);
 }
